8bec2156eb74597e2037337c34733cb2614f7960
Héctor Ramón <hector0193@gmail.com>
2015-05-28 18:58:41 +0200

    Stop enabling $by_category projection

--- event_store.sh
+++ event_store.sh
@@ -1,15 +1,5 @@
 #!/bin/sh
 
-# Enable $by_category projection
-(
-    xargs wget localhost:2113 --retry-connrefused --tries=5 -q --wait=2 --spider;
-    curl \
-        -X POST \
-        --user admin:changeit \
-        -H "Content-Length: 0" \
-        'http://127.0.0.1:2113/projection/$by_category/command/enable'
-) &
-
 # Run EventStore
 cd EventStore
 exec ./run-node.sh $@
